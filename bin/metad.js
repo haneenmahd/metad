0f9745f32ee61a27d551869789a5b04b58487e74
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-27 22:25:44 +0530

    Add CLI entry point

--- bin/metad.js
+++ bin/metad.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log("Metad binary file");
+const process = require("process");
+const chalk = require("chalk");
+const PackageMeta = require("../lib/PackageMeta");
+
+let packageName = process.argv[2];
+
+(async () => {
+  let report = await PackageMeta.generateReport(packageName);
+
+  let name = chalk.bold(`${chalk.yellow("Name")}: ${report.name}`);
+  let latestVersion = chalk.bold(
+    `${chalk.yellow("Latest version")}: ${report.version}`
+  );
+  let description = chalk.bold(
+    `${chalk.yellow("Description")}: ${report.description}`
+  );
+  let repository = chalk.bold(
+    `${chalk.yellow("Repository")}: ${report.repository}`
+  );
+  let homepage = chalk.bold(`${chalk.yellow("Homepage")}: ${report.homepage}`);
+  let score = chalk.bold(
+    `${chalk.yellow("Score")}: ${report.score.final * 100}`
+  );
+
+  console.log(name);
+  console.log(latestVersion);
+  console.log(description);
+  console.log(repository);
+  console.log(homepage);
+  console.log(score);
+})();
