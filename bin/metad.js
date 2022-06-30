c63f203189f007bfb60ca5fb080950ee00b903c5
Haneen Mahdin <haneenmahdin@gmail.com>
2022-06-30 08:04:16 +0530

    catch errors and print a meaningful message instead of Error object

--- bin/metad.js
+++ bin/metad.js
@@ -7,27 +7,38 @@ const PackageMeta = require("../lib/PackageMeta");
 let packageName = process.argv[2];
 
 (async () => {
-  let report = await PackageMeta.generateReport(packageName);
+  try {
+    let report = await PackageMeta.generateReport(packageName);
 
-  let name = chalk.bold(`${chalk.yellow("Name")}: ${report.name}`);
-  let latestVersion = chalk.bold(
-    `${chalk.yellow("Latest version")}: ${report.version}`
-  );
-  let description = chalk.bold(
-    `${chalk.yellow("Description")}: ${report.description}`
-  );
-  let repository = chalk.bold(
-    `${chalk.yellow("Repository")}: ${report.repository}`
-  );
-  let homepage = chalk.bold(`${chalk.yellow("Homepage")}: ${report.homepage}`);
-  let score = chalk.bold(
-    `${chalk.yellow("Score")}: ${report.score.final * 100}`
-  );
+    let name = chalk.bold(`${chalk.yellow("Name")}: ${report.name}`);
+    let latestVersion = chalk.bold(
+      `${chalk.yellow("Latest version")}: ${report.version}`
+    );
+    let description = chalk.bold(
+      `${chalk.yellow("Description")}: ${report.description}`
+    );
+    let repository = chalk.bold(
+      `${chalk.yellow("Repository")}: ${report.repository}`
+    );
+    let homepage = chalk.bold(
+      `${chalk.yellow("Homepage")}: ${report.homepage}`
+    );
+    let score = chalk.bold(
+      `${chalk.yellow("Score")}: ${report.score.final * 100}`
+    );
 
-  console.log(name);
-  console.log(latestVersion);
-  console.log(description);
-  console.log(repository);
-  console.log(homepage);
-  console.log(score);
+    console.log(name);
+    console.log(latestVersion);
+    console.log(description);
+    console.log(repository);
+    console.log(homepage);
+    console.log(score);
+  } catch (e) {
+    console.log(
+      chalk.red(
+        "An unexpected error occured while fetching info for package:",
+        chalk.bold(packageName)
+      )
+    );
+  }
 })();
